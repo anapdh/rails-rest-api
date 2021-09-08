0ea9f01ef02b57087f94ffaddd20224664a063e8
anapdh <ana_d_hubner@hotmail.com>
2021-09-08 19:25:46 -0300

    Add first tests

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'tests a number to be positive' do
+    expect(1).to be_positive
+    # article = Article.new(title: 'Title', content: 'Text')
+    # article.save
+    # expect(article.id).to be > 0
+  end
 end
