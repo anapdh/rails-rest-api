38326d6738347ac6c47ea246f6398e65ae0c4bb8
Ana Paula <ana_d_hubner@hotmail.com>
2021-09-22 18:38:08 -0300

    Update rspec code

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,15 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  it 'tests a number to be positive' do
-    expect(1).to be_positive
-    # article = Article.new(title: 'Title', content: 'Text')
-    # article.save
-    # expect(article.id).to be > 0
+  describe '#validations' do
+    let(:article) { build(:article) }
+    it 'tests that factory is valid' do
+      expect(article).to be_valid
+    end
+    it 'has an invalida title' do
+      article.title = ''
+      expect(article).not_to be_valid
+      expect(article.errors[:title]).to include("can't be blank")
+      # article = Article.new(title: 'Title', content: 'Text')
+      # article.save
+      # expect(article.id).to be > 0
+    end
   end
 
-  it 'tests article' do
-    article = FactoryBot.create(:article)
-    expect(article.title).to eq('Sample article')
-  end
+  # it 'tests article' do
+  #   article = FactoryBot.create(:article) # can add factorybot to configs so doesn't need to write here
+  #   expect(article.title).to eq('Sample article')
+  # end
 end
